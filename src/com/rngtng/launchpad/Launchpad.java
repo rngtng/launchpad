5fbd57e3ba31441db883ae243a72302168825bcb
Tobias Bielohlawek <tobiasb@qype.com>
2009-12-10 18:40:42 +0100

    updated to LColor and LMidiCode changes, added first approach towards Input reader

--- src/com/rngtng/launchpad/Launchpad.java
+++ src/com/rngtng/launchpad/Launchpad.java
@@ -34,7 +34,7 @@ import themidibus.*;
  * @author rngtng - Tobias Bielohlawek
  *
  */
-public class Launchpad implements MidiCodes {
+public class Launchpad implements LMidiCodes, StandardMidiListener {
 
 	PApplet app;
 	MidiBus midiBus; // The MidiBus
@@ -61,6 +61,7 @@ public class Launchpad implements MidiCodes {
 		app = _app;
 		app.registerDispose(this);
 		midiBus = new MidiBus(_app, inputName, outputName);
+		midiBus.addMidiListener(this);
 	}
 
 	public void dispose() {
@@ -96,8 +97,8 @@ public class Launchpad implements MidiCodes {
 	 *
 	 * [Launchpad::NoOutputAllowedError] when output is not enabled
 	 */
-	public void test_leds() {		
-		test_leds(Color.HIGH);
+	public void test_leds() {
+		test_leds(LColor.HIGH);
 	}
 
 	/**
@@ -123,31 +124,49 @@ public class Launchpad implements MidiCodes {
 	/**
 	 * Changes a single Control or Scene Button. Specify the Button by its name
 	 *
-	 * @param buttonName Name of the button
-	 * @param red   brightness of red LED
-	 * @param green brightness of green LED
-	 * @param mode  button mode, defaults to <tt>MODE_NORMAL</tt>, one of:
-	 *                   MODE_NORMAL   updates the LED for all circumstances (the new value will be written to both buffers)
-	 *                   MODE_FLASHING flashing   updates the LED for flashing (the new value will be written to buffer 0 while the LED will be off in buffer 1, see buffering_mode)
-	 *                   MODE_BUFFERED updates the LED for the current update_buffer only
+	 * @param buttonCode Code of the button
+	 * @param c     LColor object
 	 *
 	 * Errors raised:
-	 *
 	 * [Launchpad::NoValidGridCoordinatesError] when coordinates aren't within the valid range
 	 * [Launchpad::NoValidBrightnessError] when brightness values aren't within the valid range
 	 * [Launchpad::NoOutputAllowedError] when output is not enabled
 	 */
-	public void changeButton(String buttonName, Color c) {
-		changeButton(buttonName, c.red, c.green, c.mode);
+	public void changeButton(byte buttonCode, LColor c) {
+		changeButton(buttonCode, c.red, c.green, c.mode);
 	}
 
-	public void changeButton(String buttonName, byte red, byte green) {
-		changeButton(buttonName, red, green, Color.NORMAL);
+	/**
+	 * Changes a single Control or Scene Button. Specify the Button by its name
+	 *
+	 * @param buttonCode Code of the button
+	 * @param red   brightness of red LED
+	 * @param green brightness of green LED
+	 *
+	 * Errors raised:
+	 * [Launchpad::NoValidGridCoordinatesError] when coordinates aren't within the valid range
+	 * [Launchpad::NoValidBrightnessError] when brightness values aren't within the valid range
+	 * [Launchpad::NoOutputAllowedError] when output is not enabled
+	 */
+	public void changeButton(byte buttonCode, byte red, byte green) {
+		changeButton(buttonCode, red, green, LColor.NORMAL);
 	}
 
-	public void changeButton(String buttonName, byte red, byte green, byte mode) {
-		int status = (" up down left right session user1 user2 mixer ".indexOf(" " + buttonName + " ") >= 0) ? STATUS_CC : STATUS_ON;
-		output(status, buttonCode(buttonName), Color.velocity(red, green, mode));
+	/**
+	 * Changes a single Control or Scene Button. Specify the Button by its name
+	 *
+	 * @param buttonCode Code of the button
+	 * @param c     LColor object
+	 *
+	 * Errors raised:
+	 * [Launchpad::NoValidGridCoordinatesError] when coordinates aren't within the valid range
+	 * [Launchpad::NoValidBrightnessError] when brightness values aren't within the valid range
+	 * [Launchpad::NoOutputAllowedError] when output is not enabled
+	 */
+	public void changeButton(byte buttonCode, byte red, byte green, byte mode) {
+	    //if(!isButtonCode(buttonCode)) throw
+		int status = (isSceneButtonCode(buttonCode) ? STATUS_ON : STATUS_CC;
+		output(status, buttonCode, LColor.velocity(red, green, mode));
 	}
 
 	/**
@@ -157,13 +176,9 @@ public class Launchpad implements MidiCodes {
 	 * @param y     y coordinate
 	 * @param red   brightness of red LED
 	 * @param green brightness of green LED
-	 * @param mode  button mode, defaults to <tt>MODE_NORMAL</tt>, one of:
-	 *                   MODE_NORMAL   updates the LED for all circumstances (the new value will be written to both buffers)
-	 *                   MODE_FLASHING flashing   updates the LED for flashing (the new value will be written to buffer 0 while the LED will be off in buffer 1, see buffering_mode)
-	 *                   MODE_BUFFERED updates the LED for the current update_buffer only
-	 *
+	 * @param mode  button mode
+     *
 	 * Errors raised:
-	 *
 	 * [Launchpad::NoValidGridCoordinatesError] when coordinates aren't within the valid range
 	 * [Launchpad::NoValidBrightnessError] when brightness values aren't within the valid range
 	 * [Launchpad::NoOutputAllowedError] when output is not enabled
@@ -172,12 +187,39 @@ public class Launchpad implements MidiCodes {
 		changeGrid(x, y, c.red, c.green, c.mode);
 	}
 
+	/**
+	 * Changes a single Button on the Grid. Specify the Button by its x & y coordinates
+	 *
+	 * @param x     x coordinate
+	 * @param y     y coordinate
+	 * @param red   brightness of red LED
+	 * @param green brightness of green LED
+	 *
+	 * Errors raised:
+	 * [Launchpad::NoValidGridCoordinatesError] when coordinates aren't within the valid range
+	 * [Launchpad::NoValidBrightnessError] when brightness values aren't within the valid range
+	 * [Launchpad::NoOutputAllowedError] when output is not enabled
+	 */
 	public void changeGrid(int x, int y, byte red, byte green) {
-		changeGrid(x, y, red, green, Color.NORMAL);
+		changeGrid(x, y, red, green, LColor.NORMAL);
 	}
 
+	/**
+	 * Changes a single Button on the Grid. Specify the Button by its x & y coordinates
+	 *
+	 * @param x     x coordinate
+	 * @param y     y coordinate
+	 * @param red   brightness of red LED
+	 * @param green brightness of green LED
+	 * @param mode  button mode
+	 *
+	 * Errors raised:
+	 * [Launchpad::NoValidGridCoordinatesError] when coordinates aren't within the valid range
+	 * [Launchpad::NoValidBrightnessError] when brightness values aren't within the valid range
+	 * [Launchpad::NoOutputAllowedError] when output is not enabled
+	 */
 	public void changeGrid(int x, int y, byte red, byte green, byte mode) {
-		output(STATUS_ON, (y * 16 + x), Color.velocity(red, green, mode));
+		output(STATUS_ON, (y * 16 + x), LColor.velocity(red, green, mode));
 	}
 
 	/**
@@ -195,7 +237,7 @@ public class Launchpad implements MidiCodes {
 	 */
 	public void change_all(Color[] colors) {
 		byte param1, param2;
-		
+
 		// send normal MIDI message to reset rapid LED change pointer
 		//  in this case, set mapping mode to x-y layout (the default)
 		output(STATUS_CC, STATUS_NIL, GRIDLAYOUT_XY);
@@ -203,7 +245,7 @@ public class Launchpad implements MidiCodes {
 		//  send colors in slices of 2
 		for(int i = 0; i < 80; i = i + 2) {
 			try {
-				param1 = colors[i].to_byte();
+				param1 = colors[i].velocity();
 			} catch (ArrayIndexOutOfBoundsException e) {
 				param1 = 0;
 			} catch (NullPointerException e) {
@@ -211,14 +253,14 @@ public class Launchpad implements MidiCodes {
 			}
 
 			try {
-				param2 = colors[i+1].to_byte();
+				param2 = colors[i+1].velocity();
 			}   catch (ArrayIndexOutOfBoundsException e) {
 				param2 = 0;
 			}  catch (NullPointerException e) {
 				param2 = 0;
 			}
 			output(STATUS_MULTI, param1, param2);
-		}	
+		}
 	}
 
 	/**
@@ -229,7 +271,6 @@ public class Launchpad implements MidiCodes {
 	 * @param colors an array of bytes
 	 *
 	 * Errors raised:
-	 *
 	 * [Launchpad::NoValidBrightnessError] when brightness values aren't within the valid range
 	 * [Launchpad::NoOutputAllowedError] when output is not enabled
 	 */
@@ -258,10 +299,19 @@ public class Launchpad implements MidiCodes {
 			}  catch (NullPointerException e) {
 				param2 = 0;
 			}
-			output(STATUS_MULTI, param1, param2);			
+			output(STATUS_MULTI, param1, param2);
 		}
 	}
 
+	/**
+	 * Changes all buttons in batch mode. Using the pixel data of an PImage
+	 *
+	 * @param image an PImage object
+	 *
+	 * Errors raised:
+	 * [Launchpad::NoValidBrightnessError] when brightness values aren't within the valid range
+	 * [Launchpad::NoOutputAllowedError] when output is not enabled
+	 */
 	public void change_all(PImage image) {
 		byte param1, param2;
 
@@ -297,7 +347,6 @@ public class Launchpad implements MidiCodes {
 	 * Switches LEDs marked as flashing on when using custom timer for flashing.
 	 *
 	 * Errors raised:
-	 *
 	 * [Launchpad::NoOutputAllowedError] when output is not enabled
 	 */
 	public void flashing_on() {
@@ -320,7 +369,6 @@ public class Launchpad implements MidiCodes {
 	 * Stop flashing by calling flashing_on or flashing_off.
 	 *
 	 * Errors raised:
-	 *
 	 * [Launchpad::NoOutputAllowedError] when output is not enabled
 	 */
 	public void flashing_auto()  {
@@ -343,7 +391,6 @@ public class Launchpad implements MidiCodes {
 	 * @param flashing       whether to start flashing by automatically switching between the two buffers for display, <tt>true/false</tt>, defaults to <tt>false</tt>
 	 *
 	 * Errors raised:
-	 *
 	 * [Launchpad::NoOutputAllowedError] when output is not enabled
 	 */
 	public void buffering_mode(int display_buffer, int update_buffer, boolean copy, boolean flashing) {
@@ -354,59 +401,96 @@ public class Launchpad implements MidiCodes {
 	}
 
 
-	// Writes several messages to the MIDI device.
-	//
-	// Parameters:
-	//
-	// [+messages+]  an array of hashes (usually created with message) with:
-	//                 @param message  an array of
-	//                                       MIDI status code,
-	//                                       MIDI data 1 (note),
-	//                                       MIDI data 2 (velocity)
-	//                 @param timestampinteger indicating the time when the MIDI message was created
+    public void midiMessage(MidiMessage message) { // You can also use midiMessage(MidiMessage message, String bus_name)
+    	// Receive a MidiMessage
+    	// MidiMessage is an abstract class, the actual passed object will be either javax.sound.midi.MetaMessage, javax.sound.midi.ShortMessage, javax.sound.midi.SysexMessage.
+    	// Check it out here http://java.sun.com/j2se/1.5.0/docs/api/javax/sound/midi/package-summary.html
+    	app.println();
+    	app.println("MidiMessage Data:");
+    	app.println("--------");
+    	app.println("Status Byte/MIDI Command:"+message.getStatus());
+    	if(message.getMessage().length > 1) app.println("Param 1: "+(int)(message.getMessage()[1] & 0xFF));
+    	if(message.getMessage().length > 2) app.println("Param 2: "+(int)(message.getMessage()[2] & 0xFF));
+    }
+
+    /**
+    * Reads user actions (button presses/releases) that haven't been handled yet.
+    *
+    * Returns:
+    *
+    * an array of hashes with (see Launchpad for values):
+    *
+    * [<tt>:timestamp</tt>] integer indicating the time when the action occured
+    * [<tt>:state</tt>]     state of the button after action
+    * [<tt>:type</tt>]      type of the button
+    * [<tt>:x</tt>]         x coordinate
+    * [<tt>:y</tt>]         y coordinate
+    *
+    * Errors raised:
+    * [Launchpad::NoInputAllowedError] when input is not enabled
+    */
+    private void read_pending_actions(MidiMessage midi_message) {
+        int code     = message.getStatus();
+        int note     = message.getMessage()[1] & 0xFF;
+        int velocity = message.getMessage()[2] & 0xFF;
+
+        if( status == Status::ON ) {
+            r = (velocity == 127) ? GridDownEvent(note % 16, note / 16) : GridUpEvent(note % 16, note / 16);
+        }
+        if( status == Status::CC || (status == Status::ON && isSceneButtonCode(note)) ) {
+            r = (velocity == 127) ? ButtonDownEvent(note) : ButtonUpEvent(note);
+        }
+    }
+
+	/** Writes a messages to the MIDI device.
+	 *
+     */
 	private void output(int status, byte data1, byte data2) {
-		//raise NoOutputAllowedError if @output.nil?		
+		//raise NoOutputAllowedError if @output.nil?
 		midiBus.sendMessage(new byte[]{(byte) status, data1, data2});
 	}
 
+	/** Writes a messages to the MIDI device.
+	 *
+     */
 	private void output(int status, int data1, int data2) {
-		//raise NoOutputAllowedError if @output.nil?		
+		//raise NoOutputAllowedError if @output.nil?
 		midiBus.sendMessage(new byte[]{ (byte) status, (byte) data1, (byte) data2});
-	}	
-	
+	}
+
 	/**
-	 *  map the button name to button code
+	 *  checks for valid button code
 	 *
-	 *  Parameters (see Launchpad for values):
-	 *
-	 *  [+button+] name of the button
-	 *
-	 *  Returns:
-	 *
-	 *  integer to be used for MIDI data 1
-	 *
-	 *  Errors raised:
-	 *
-	 *  [Launchpad::NoValidGridCoordinatesError] when coordinates aren't within the valid range
+	 *  @param buttonCode code of the button
+	 *  @return boolean wether code is valid
 	 */
-	private byte buttonCode(String button_name) {
-		if(button_name == "up"     ) return CONTROLBUTTON_UP;
-		if(button_name == "down"   ) return CONTROLBUTTON_DOWN;
-		if(button_name == "left"   ) return CONTROLBUTTON_LEFT;
-		if(button_name == "right"  ) return CONTROLBUTTON_RIGHT;
-		if(button_name == "session") return CONTROLBUTTON_SESSION;
-		if(button_name == "user1"  ) return CONTROLBUTTON_USER1;
-		if(button_name == "user2"  ) return CONTROLBUTTON_USER2;
-		if(button_name == "mixer"  ) return CONTROLBUTTON_MIXER;
-		if(button_name == "scene1" ) return SCENEBUTTON_SCENE1;
-		if(button_name == "scene2" ) return SCENEBUTTON_SCENE2;
-		if(button_name == "scene3" ) return SCENEBUTTON_SCENE3;
-		if(button_name == "scene4" ) return SCENEBUTTON_SCENE4;
-		if(button_name == "scene5" ) return SCENEBUTTON_SCENE5;
-		if(button_name == "scene6" ) return SCENEBUTTON_SCENE6;
-		if(button_name == "scene7" ) return SCENEBUTTON_SCENE7;
-		if(button_name == "scene8" ) return SCENEBUTTON_SCENE8;
-		return 0;
-		//raise NoValidGridCoordinatesError.new("you need to specify valid coordinates (x/y, 0-7, from top left), you specified: x=#{x}, y=#{y}") if x < 0 || x > 7 || y < 0 || y > 7
+	private boolean isButtonCode(byte buttonCode) {
+		if(buttonCode == BUTTON_UP)      return true;
+		if(buttonCode == BUTTON_DOWN)    return true;
+		if(buttonCode == BUTTON_LEFT)    return true;
+		if(buttonCode == BUTTON_RIGHT)   return true;
+		if(buttonCode == BUTTON_SESSION) return true;
+		if(buttonCode == BUTTON_USER1)   return true;
+		if(buttonCode == BUTTON_USER2)   return true;
+		if(buttonCode == BUTTON_MIXER)   return true;
+		return isSceneButtonCode(buttonCode);
+	}
+
+	/**
+	 *  checks for valid scene button code
+	 *
+	 *  @param buttonCode code of the button
+	 *  @return boolean wether code is as scene button
+	 */
+	private boolean isSceneButtonCode(byte buttonCode) {
+		if(buttonCode == BUTTON_SCENE1) return true;
+		if(buttonCode == BUTTON_SCENE2) return true;
+		if(buttonCode == BUTTON_SCENE3) return true;
+		if(buttonCode == BUTTON_SCENE4) return true;
+		if(buttonCode == BUTTON_SCENE5) return true;
+		if(buttonCode == BUTTON_SCENE6) return true;
+		if(buttonCode == BUTTON_SCENE7) return true;
+		if(buttonCode == BUTTON_SCENE8) return true;
+		return false;
 	}
 }
